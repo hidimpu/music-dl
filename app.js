e0ac33c1c592cb5e42795474610a73aa53104821
parthds450 <parthds450@gmail.com>
2021-05-10 11:23:13 +0530

    minor Changes 👀

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var port = process.env.PORT || 9000
-require('dotenv').config()
-const path = require('path')
-const cors = require('cors')
-const express = require('express')
+var port = process.env.PORT || 9000;
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
 
-const pub = require('./routes/public')
-const api = require('./routes/api')
+const pub = require("./routes/public");
+const api = require("./routes/api");
 
-var app = express()
+var app = express();
 
-app.use('/', express.static(path.join(__dirname, '/public')))
-app.use(cors())
-app.use('/', pub)
-app.use('/api', api)
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(cors());
+app.use("/", pub);
+app.use("/api", api);
 
 app.listen(port, () => {
-  console.log(`MUSIC-DL SERVER IS UP ON PORT ${port}`)
-})
+  console.log(`MUSIC-DL SERVER IS UP! ON PORT ${port} Enjoy! :)  `);
+});
